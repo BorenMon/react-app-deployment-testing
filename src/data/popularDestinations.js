ef089438cee8132c7bffa9b94c67a1bf5f23f218
Mon Boren <borenmon5@gmail.com>
2021-10-26 17:05:09 +0700

    *

---
+++ src/data/popularDestinations.js
@@ -0,0 +1,46 @@
+const popularDestinations = [
+  {
+    city: "Toronto",
+    averagePrice: 120,
+    propertyCount: 76,
+    imageFileName: "toronto.jpg",
+    imageAlt: "Toronto skyline",
+  },
+  {
+    city: "Malibu",
+    averagePrice: 215,
+    propertyCount: 43,
+    imageFileName: "malibu.jpg",
+    imageAlt: "Cliff in Malibu",
+  },
+  {
+    city: "Chicago",
+    averagePrice: 130,
+    propertyCount: 115,
+    imageFileName: "chicago.jpg",
+    imageAlt: "Chicago skyline",
+  },
+  {
+    city: "Seattle",
+    averagePrice: 135,
+    propertyCount: 63,
+    imageFileName: "seattle.jpg",
+    imageAlt: "Seattle skyline",
+  },
+  {
+    city: "Colorado",
+    averagePrice: 85,
+    propertyCount: 47,
+    imageFileName: "colorado.jpg",
+    imageAlt: "Lake in Colorado",
+  },
+  {
+    city: "Miami",
+    averagePrice: 115,
+    propertyCount: 86,
+    imageFileName: "miami.jpg",
+    imageAlt: "Beach in Miami",
+  },
+];
+
+export default popularDestinations
